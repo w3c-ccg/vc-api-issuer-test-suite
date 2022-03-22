fdab3b6746ada811cf7e9f199c35c646a117d2e5
Andrew Jones <ajones@digitalbazaar.com>
2022-03-22 10:56:02 -0600

    Add issue command to imlementation.

---
+++ tests/Implementation.js
@@ -0,0 +1,31 @@
+const httpClient = require('@digitalbazaar/http-client');
+const https = require('https');
+
+const agent = new https.Agent({rejectUnauthorized: false});
+
+/**
+ * Issues a VC from an endpoint.
+ *
+ * @param {object} options - Options to use.
+ * @param {object} options.body - The body for the request.
+ * @param {object} [options.headers={}] - Headers for the request.
+ * @param {string} options.endpoint - The issuer endpoint.
+ *
+ * @returns {Promise<object>} Contains the result and error.
+ */
+const issue = async ({body, headers = {}, endpoint}) => {
+  let result;
+  let error;
+    try {
+      result = await httpClient.post(
+        `${endpoint}/credentials/issue`,
+          {
+            json: body,
+            agent,
+            headers: {...headers}
+          });
+    } catch(e) {
+      error = e;
+    }
+  return {result, error};
+}
